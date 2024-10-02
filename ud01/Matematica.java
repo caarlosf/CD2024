78855d542cc89e7d6e896bfc1ecef4b3198968ad
Carlos <1999cfo@gmail.com>
2024-10-02 10:20:54 +0200

    02-10-24

---
+++ ud01/Matematica.java
@@ -0,0 +1,23 @@
+//Crear programa que:
+// calcule raíz cuadrada de 234324
+// calcule 34 elevado a 17 
+
+/**
+ * Matematica
+ */
+public class Matematica {
+
+    public static void main(String[] args) {
+        
+        double raiz;
+        raiz = Math.sqrt(234324);
+        
+        System.out.println("Raíz: " + raiz);
+    
+        double potencia;
+        potencia = Math.pow(34, 17);
+
+        System.out.println("Potencia: " + potencia);
+    
+    }
+}
