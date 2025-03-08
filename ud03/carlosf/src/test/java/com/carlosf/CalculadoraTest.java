aa51c0140a7f5cf3fc8ff9e5c6c39357a102144a
Carlos <1999cfo@gmail.com>
2025-03-08 03:07:22 +0100

    proba

--- ud03/carlosf/src/test/java/com/carlosf/CalculadoraTest.java
+++ ud03/carlosf/src/test/java/com/carlosf/CalculadoraTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-public class CalculadoraTest implements ThrowingRunnable{
+public class CalculadoraTest implements ThrowingRunnable{2
 
     private final Calculadora calculadora = new Calculadora();
 
