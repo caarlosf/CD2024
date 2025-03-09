e0fc523e3c37e7275b98b461e1c50d4c7c421126
Carlos <1999cfo@gmail.com>
2025-03-09 03:12:51 +0100

    tdd ValidadorContrasinal

---
+++ ud03/tdd/src/main/java/com/tdd/ValidadorContrasena.java
@@ -0,0 +1,38 @@
+package com.tdd;
+
+public class ValidadorContrasena {
+
+    public boolean esValida(String contraseña) {
+        if (contraseña == null || contraseña.length() < 8) {
+            return false;
+        }
+
+        boolean tieneMayuscula = false;
+        boolean tieneMinuscula = false;
+        boolean tieneDigito    = false;
+        boolean tieneEspecial  = false;
+
+
+        String especiales = "!@#$%^&*";
+
+
+        for (char c : contraseña.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                tieneMayuscula = true;
+            }
+            if (Character.isLowerCase(c)) {
+                tieneMinuscula = true;
+            }
+            if (Character.isDigit(c)) {
+                tieneDigito = true;
+            }
+
+            if (especiales.indexOf(c) != -1) {
+                tieneEspecial = true;
+            }
+        }
+
+
+        return tieneMayuscula && tieneMinuscula && tieneDigito && tieneEspecial;
+    }
+}
